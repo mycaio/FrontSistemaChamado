e5e271a4f1343f3e31a4172016c9959f3bdb817b
caio.mendonca <caio.mendonca@edu.unifil.br>
2026-06-17 18:26:47 -0300

    subindo melhorias na arquitetura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-black font-bold text-center">Ola Mundo</h1>
-    </>
-  );
+  return <Dashboard />;
 }
 
 export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Sistema de Chamados</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
